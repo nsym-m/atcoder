dbd0c99a028c7a13d654a0e8aaa72b9b74c3b46d
nishiyama <nishiyama_mikiya@andfactory.co.jp>
2023-06-04 19:47:47 +0900

    add: abs abc088_b

---
+++ contests/abs/abc088_b/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+var scanner *bufio.Scanner
+
+func main() {
+
+	in := readInt()
+	in2 := readInts()
+	var a, b int
+	sort.Sort(sort.Reverse(sort.IntSlice(in2)))
+	for i := 0; i < in; i++ {
+		if i%2 == 0 {
+			a += in2[i]
+		} else {
+			b += in2[i]
+		}
+	}
+	fmt.Println(a - b)
+}
+
+// --- init
+
+func init() {
+	scanner = bufio.NewScanner(os.Stdin)
+}
+
+func readInt() int {
+	scanner.Scan()
+	i, err := strconv.Atoi(scanner.Text())
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func readString() string {
+	scanner.Scan()
+	return scanner.Text()
+}
+
+func readInts() []int {
+	scanner.Scan()
+	inputStr := scanner.Text()
+	inputStrs := strings.Split(inputStr, " ")
+
+	inputs := make([]int, len(inputStrs))
+	for i, inputStr := range inputStrs {
+		input, err := strconv.Atoi(inputStr)
+		if err != nil {
+			panic(err)
+		}
+		inputs[i] = input
+	}
+
+	return inputs
+}
