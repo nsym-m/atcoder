066586e94e1c6c56bc1bcf5d000819e2821a0b57
nishiyama <nishiyama_mikiya@andfactory.co.jp>
2024-01-03 02:27:51 +0900

    wip: abc330_b,c

---
+++ contests/abc330/abc330_c/main.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+var scanner *bufio.Scanner
+
+func main() {
+
+	in := readInt()
+	var res, x, y int
+	for {
+		a := (x*x + y*y) - in
+		if in < 30 {
+			fmt.Println("a")
+			fmt.Println(a)
+			fmt.Println(x * x)
+			fmt.Println(y * y)
+			fmt.Println((x*x + y*y))
+		}
+		if a >= 0 {
+			res = a
+			break
+		}
+		for {
+			aa := (x*x + y*y) - in
+			if in < 30 {
+				fmt.Println("aa")
+				fmt.Println(aa)
+				fmt.Println(x * x)
+				fmt.Println(y * y)
+				fmt.Println((x*x + y*y))
+			}
+			if aa >= 0 {
+				res = aa
+				break
+			}
+			y++
+		}
+		x++
+	}
+
+	fmt.Println(res)
+}
+
+// --- init
+
+func init() {
+	scanner = bufio.NewScanner(os.Stdin)
+}
+
+func readInt() int {
+	scanner.Scan()
+	i, err := strconv.Atoi(scanner.Text())
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func readString() string {
+	scanner.Scan()
+	return scanner.Text()
+}
+
+func readInts() []int {
+	scanner.Scan()
+	inputStr := scanner.Text()
+	inputStrs := strings.Split(inputStr, " ")
+
+	inputs := make([]int, len(inputStrs))
+	for i, inputStr := range inputStrs {
+		input, err := strconv.Atoi(inputStr)
+		if err != nil {
+			panic(err)
+		}
+		inputs[i] = input
+	}
+
+	return inputs
+}
+
+func int2() (int, int) {
+	ints := readInts()
+	return ints[0], ints[1]
+}
+
+func sortDesc(ints []int) []int {
+	sort.Sort(sort.Reverse(sort.IntSlice(ints)))
+	return ints
+}
+
+func reverseString(s string) string {
+	rs := []rune(s)
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		rs[i], rs[j] = rs[j], rs[i]
+	}
+	return string(rs)
+}
+
+func iToS(i int) string {
+	return strconv.Itoa(i)
+}
+
+func sToI(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+type Queue struct {
+	data [][2]int
+	size int
+}
+
+func NewQueue(len int) *Queue {
+	return &Queue{
+		data: make([][2]int, len, 0),
+		size: 0,
+	}
+}
+
+// キューにデータを追加する
+func (q *Queue) Push(i [2]int) {
+	q.data = append(q.data, i)
+	q.size++
+}
+
+// キューの最新データを削除
+func (q *Queue) Pop() bool {
+	if q.IsEmpty() {
+		return false
+	}
+	q.size--
+	q.data = q.data[1:]
+	return true
+}
+
+// キューの最新データを取得
+func (q *Queue) Front() [2]int {
+	return q.data[0]
+}
+
+func (q *Queue) IsEmpty() bool {
+	return q.size == 0
+}
+
+func (q *Queue) String() string {
+	return fmt.Sprint(q.data)
+}
