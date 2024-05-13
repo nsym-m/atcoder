9c090e5ee9f6ebf3d05b6ffd31a797b7833110ff
nishiyama <nishiyama_mikiya@andfactory.co.jp>
2024-05-13 23:46:09 +0900

    add: abc347_b

---
+++ contests/abc347/abc347_b/main.go
@@ -0,0 +1,172 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+var scanner *bufio.Scanner
+
+func main() {
+
+	s := readString()
+	m := make(map[string]struct{}, 0)
+	for i, _ := range s {
+		for j, _ := range s[i:] {
+			ss := s[i : i+j+1]
+			if _, ok := m[ss]; !ok {
+				m[ss] = struct{}{}
+			}
+		}
+	}
+
+	fmt.Println(len(m))
+}
+
+// --- init
+
+func init() {
+	scanner = bufio.NewScanner(os.Stdin)
+}
+
+func readInt() int {
+	scanner.Scan()
+	i, err := strconv.Atoi(scanner.Text())
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func readString() string {
+	scanner.Scan()
+	return scanner.Text()
+}
+
+func readInts() []int {
+	scanner.Scan()
+	inputStr := scanner.Text()
+	inputStrs := strings.Split(inputStr, " ")
+
+	inputs := make([]int, len(inputStrs))
+	for i, inputStr := range inputStrs {
+		input, err := strconv.Atoi(inputStr)
+		if err != nil {
+			panic(err)
+		}
+		inputs[i] = input
+	}
+
+	return inputs
+}
+
+func int2() (int, int) {
+	ints := readInts()
+	return ints[0], ints[1]
+}
+
+func sortDesc(ints []int) {
+	sort.Sort(sort.Reverse(sort.IntSlice(ints)))
+}
+
+func sortAsc(ints []int) {
+	sort.Ints(ints)
+}
+
+func reverseString(s string) string {
+	rs := []rune(s)
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		rs[i], rs[j] = rs[j], rs[i]
+	}
+	return string(rs)
+}
+
+func iToS(i int) string {
+	return strconv.Itoa(i)
+}
+
+func sToI(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func maxu(a, b uint64) uint64 {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func print(a ...any) {
+	for _, v := range a {
+		fmt.Printf("%+v\n", v)
+	}
+}
+
+type Queue struct {
+	data [][2]int
+	size int
+}
+
+func NewQueue(len int) *Queue {
+	return &Queue{
+		data: make([][2]int, len, 0),
+		size: 0,
+	}
+}
+
+// キューにデータを追加する
+func (q *Queue) Push(i [2]int) {
+	q.data = append(q.data, i)
+	q.size++
+}
+
+// キューの最新データを削除
+func (q *Queue) Pop() bool {
+	if q.IsEmpty() {
+		return false
+	}
+	q.size--
+	q.data = q.data[1:]
+	return true
+}
+
+// キューの最新データを取得
+func (q *Queue) Front() [2]int {
+	return q.data[0]
+}
+
+func (q *Queue) IsEmpty() bool {
+	return q.size == 0
+}
+
+func (q *Queue) String() string {
+	return fmt.Sprint(q.data)
+}
+
+// ユークリッド距離
+func euclidean(x1, x2, y1, y2 int) float64 {
+	return math.Sqrt(math.Pow(float64(x1-x2), 2) + math.Pow(float64(y1-y2), 2))
+}
